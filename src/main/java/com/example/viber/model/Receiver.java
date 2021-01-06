f37ac638b06836b6cab6bc14e6983982b1ed8202
Skajl-dev <bilonigka19999@gmail.com>
2021-01-06 21:22:53 +0200

    db test 4

--- src/main/java/com/example/viber/model/Receiver.java
+++ src/main/java/com/example/viber/model/Receiver.java
@@ -8,15 +8,8 @@ public class Receiver {
     @Id
     private String id;
     private String name;
-    private String email;
 
     public Receiver() {}
-
-    public Receiver(String id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
 
     public Receiver(String id, String name) {
         this.id = id;
