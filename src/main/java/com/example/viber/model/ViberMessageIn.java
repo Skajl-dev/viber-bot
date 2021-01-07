2a1794136334481df2fa56e02a96ced26bd35d21
Skajl-dev <bilonigka19999@gmail.com>
2021-01-07 13:05:03 +0200

    db test 16

--- src/main/java/com/example/viber/model/ViberMessageIn.java
+++ src/main/java/com/example/viber/model/ViberMessageIn.java
@@ -4,8 +4,6 @@ public class ViberMessageIn {
     private EventTypes event;
     private Sender sender;
     private Message message;
-    private String context;
-    private String user_id;
 
     public ViberMessageIn() {}
 
@@ -31,21 +29,5 @@ public class ViberMessageIn {
 
     public void setMessage(Message message) {
         this.message = message;
-    }
-
-    public String getContext() {
-        return context;
-    }
-
-    public void setContext(String context) {
-        this.context = context;
-    }
-
-    public String getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
     }
 }
