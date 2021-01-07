db88e5796a22bf64b0258540788309622728830b
Skajl-dev <bilonigka19999@gmail.com>
2021-01-07 14:36:02 +0200

    enable scheduling

--- src/main/java/com/example/viber/ViberApplication.java
+++ src/main/java/com/example/viber/ViberApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("com.example.viber.*")
+@EnableScheduling
 public class ViberApplication  {
 
 	public static void main(String[] args) {
